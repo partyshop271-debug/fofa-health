fed99f835f92304ca05ec26c7e7d207f6adc7ba6
partyshop271-debug <partyshop271@gmail.com>
2026-06-07 17:03:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
+```javascript
 const CACHE_NAME = 'boosy-sweets-pwa-v3';
-const assets = ['./', './index.html'];
+const assets = ['./', './index.html', 'https://res.cloudinary.com/dyx4w0dr1/image/upload/v1780839970/1780839705442_1_hwb7pq.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
@@ -8,3 +9,5 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
+
+
